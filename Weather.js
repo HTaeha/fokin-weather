7afe72d6aaf366a62d9f963680826f34e96231be
HTaeha <xogk39@gmail.com>
2020-11-16 19:59:00 +0900

    TabView added

--- Weather.js
+++ Weather.js
@@ -1,8 +1,9 @@
 import React from "react"
-import { View, Text, StyleSheet, StatusBar } from "react-native"
+import { View, Text, StyleSheet, StatusBar, Dimensions } from "react-native"
 import PropTypes from "prop-types"
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { LinearGradient } from 'expo-linear-gradient'
+import { TabView } from 'react-native-tab-view'
 
 const weatherOptions = {
     Thunderstorm: {
@@ -15,7 +16,7 @@ const weatherOptions = {
       iconName: "weather-hail",
       gradient: ["#89F7FE", "#66A6FF"],
       title: "Drizzle",
-      subtitle: "Is like rain, but gay 🏳️‍🌈"
+      subtitle: "Is like rain 🏳️‍🌈"
     },
     Rain: {
       iconName: "weather-rainy",
@@ -27,23 +28,25 @@ const weatherOptions = {
       iconName: "weather-snowy",
       gradient: ["#7DE2FC", "#B9B6E5"],
       title: "Cold as balls",
-      subtitle: "Do you want to build a snowman? Fuck no."
+      subtitle: "Do you want to build a snowman?" 
     },
     Atmosphere: {
       iconName: "weather-hail",
-      gradient: ["#89F7FE", "#66A6FF"]
+      gradient: ["#89F7FE", "#66A6FF"],
+      title: "Atmosphere",
+      subtitle: "I don't know"
     },
     Clear: {
       iconName: "weather-sunny",
       gradient: ["#FF7300", "#FEF253"],
-      title: "Sunny as fuck",
-      subtitle: "Go get your ass burnt"
+      title: "Sunny",
+      subtitle: "The weather is very good!"
     },
     Clouds: {
       iconName: "weather-cloudy",
       gradient: ["#D7D2CC", "#304352"],
       title: "Clouds",
-      subtitle: "I know, fucking boring"
+      subtitle: "I know, very boring"
     },
     Mist: {
       iconName: "weather-hail",
@@ -55,7 +58,7 @@ const weatherOptions = {
       iconName: "weather-hail",
       gradient: ["#4DA0B0", "#D39D38"],
       title: "Dusty",
-      subtitle: "Thanks a lot China 🖕🏻"
+      subtitle: "Thanks a lot china."
     },
     Haze: {
       iconName: "weather-hail",
@@ -63,25 +66,97 @@ const weatherOptions = {
       title: "Haze",
       subtitle: "Just don't go outside."
     }
-  }
+}
 
-export default function Weather({ temp, condition }) {
-    return (
-        <LinearGradient
-            // Button Linear Gradient
-            colors={weatherOptions[condition].gradient}
-            style={styles.container}>
-            <StatusBar barStyle="light-content" />
-            <View style={styles.halfConttainer}>
-                <MaterialCommunityIcons name={weatherOptions[condition].iconName} size={96} color="white" />
-                <Text style={styles.temp}>{temp}°</Text>
-            </View>
-            <View style={ styles.textContainer }>
-    <Text style={styles.title}>{weatherOptions[condition].title}</Text>
-    <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
-            </View>
-        </LinearGradient>
-    )
+const FirstRoute = ({temp, condition}) => (
+    <LinearGradient
+        colors={weatherOptions[condition].gradient}
+        style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.halfConttainer}>
+            <MaterialCommunityIcons name={weatherOptions[condition].iconName} size={96} color="white" />
+            <Text style={styles.temp}>{temp}°</Text>
+        </View>
+        <View style={styles.textContainer}>
+            <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+            <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
+        </View>
+    </LinearGradient>
+)
+
+const SecondRoute = () => (
+    <LinearGradient
+        colors={weatherOptions["Thunderstorm"].gradient}
+        style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.halfConttainer}>
+            <MaterialCommunityIcons name={weatherOptions["Thunderstorm"].iconName} size={96} color="white" />
+            <Text style={styles.temp}>24°</Text>
+        </View>
+        <View style={styles.textContainer}>
+            <Text style={styles.title}>{weatherOptions["Thunderstorm"].title}</Text>
+            <Text style={styles.subtitle}>{weatherOptions["Thunderstorm"].subtitle}</Text>
+        </View>
+    </LinearGradient>
+)
+
+const ThridRoute = () => (
+    <LinearGradient
+        colors={weatherOptions["Snow"].gradient}
+        style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.halfConttainer}>
+            <MaterialCommunityIcons name={weatherOptions["Snow"].iconName} size={96} color="white" />
+            <Text style={styles.temp}>365°</Text>
+        </View>
+        <View style={styles.textContainer}>
+            <Text style={styles.title}>{weatherOptions["Snow"].title}</Text>
+            <Text style={styles.subtitle}>{weatherOptions["Snow"].subtitle}</Text>
+        </View>
+    </LinearGradient>
+)
+
+const initialLayout = {
+    width: Dimensions.get('window').width
+}
+
+//export default function Weather({ temp, condition }) {
+export default class Weather extends React.Component {
+    handleIndexChange = index => {
+        this.setState({ index });
+    }
+    state = {
+        index: 0,
+        routes: [
+            { key: 'first', title: 'hello' },
+            { key: 'second', title: 'world' },
+            { key: 'third', title: 'man' }
+        ],
+    };
+
+    renderScene = ({ route }) => {
+        switch (route.key) {
+            case 'first':
+                return <FirstRoute temp={this.props.temp} condition={this.props.condition} />
+            case 'second':
+                return <SecondRoute />
+            case 'third':
+                return <ThridRoute />
+            default:
+                return null
+        }
+    }
+
+    render() {
+        return (
+            <TabView
+                navigationState={this.state}
+                renderScene={this.renderScene}
+                onIndexChange={this.handleIndexChange}
+                initialLayout={initialLayout}
+            />
+        )
+    }
 }
 
 Weather.propTypes = {
@@ -132,5 +207,4 @@ const styles = StyleSheet.create({
         alignItems: "flex-start",
         justifyContent: "center"
     }
-
 })
